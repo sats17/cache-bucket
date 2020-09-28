b1d518d3d451e645feb40deb05844c43e12f7a3e
sats17 <satishkumbhar9999@gmail.com>
2020-09-28 12:13:47 +0530

    Fix code smell for Constants.java

--- src/main/java/com/sats/caching/internal/services/Constants.java
+++ src/main/java/com/sats/caching/internal/services/Constants.java
@@ -6,6 +6,8 @@ package com.sats.caching.internal.services;
  *
  */
 class Constants {
+	
+	private Constants() {}
 
 	public static final long CACHE_EXPIRY_TIME_LIMIT = 300; 
 	public static final long SCHEDULAR_INTIAL_DELAY = 0;
