695634fb47769a64cf9c64ab70e4fbf46238935d
sats17 <satishkumbhar9999@gmail.com>
2020-10-11 10:36:06 +0530

    Added few integration test cases

--- src/test/java/com/sats/JavaSimpleInMemoryCache/AppTest.java
+++ src/test/java/com/sats/JavaSimpleInMemoryCache/AppTest.java
@@ -1,8 +1,12 @@
 package com.sats.JavaSimpleInMemoryCache;
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 import com.sats.caching.extern.CacheBucket;
 import com.sats.caching.internal.services.CacheController;
@@ -13,42 +17,57 @@ import com.sats.caching.internal.services.CacheController;
 public class AppTest {
 
 	public static void main(String[] args) throws InterruptedException {
-		System.out.println("Running started");
-		CacheBucket controller2 = new CacheController(200,10000);
-		CacheBucket controller = new CacheController(3,10000);
-		String ab = "  ";
-		String bc = null;
-		String cd = "";
-		System.out.println(ab.isBlank());
-//		System.out.println(bc.isBlank());
-		System.out.println(cd.isBlank());
-		controller.setCache("first", "abc");
-		Thread.sleep(1);
-		controller.setCache("second", "bcd");
-		Thread.sleep(10);
-		controller.setCache("third", "xyz");
-		controller.setCache("fourth", "qwe");
-		System.out.println(controller.getBucketCapacity());
-		controller.setBucketCapacity(5);
-		controller.setCache("five", "12");
-		controller.setCache("six", "12355");
-		controller.setCache("seven", "5323");
-		Thread.sleep(9000);
-		System.out.println(controller.getAll());
-		
-		System.out.println(controller.getBucketCapacity());
-		Thread.sleep(2000);
-		System.out.println(controller.getAll());
-		controller.setCache("first", "1");
-		controller.setCache("second", "1");
-		controller.setCache("third", "1");
-		controller.setCache("four", "1");
-		controller2.setCache("five", "1");
-		controller2.setCache("first", "1");
+		System.out.println("Running started"); 
+		ConcurrentMap<String, Object> ab = new ConcurrentHashMap<>(2);
+		ab.put("ax", "assas");
+		ab.put("asf", "asfas");
+		System.out.println(ab);
+		List<String> abv = new ArrayList<>();
+		abv.add("first");
+		abv.add("second");
+		abv.add("third");
+		System.out.println(abv);
+		abv.remove(0);
+		System.out.println(abv);
+		abv.remove(0);
+		System.out.println(abv);
+//		CacheBucket controller2 = new CacheController(200,10000);
+//		CacheBucket controller = new CacheController(3,10000);
+//		String ab = "  ";
+//		String bc = null;
+//		String cd = "";
+//		System.out.println(ab.isBlank());
+////		System.out.println(bc.isBlank());
+//		System.out.println(cd.isBlank());
+//		controller.setCache("first", "abc");
+//		Thread.sleep(1);
+//		controller.setCache("second", "bcd");
+//		Thread.sleep(10);
+//		controller.setCache("third", "xyz");
+//		controller.setCache("fourth", "qwe");
+//		System.out.println(controller.getBucketCapacity());
+//		controller.setBucketCapacity(5);
+//		controller.setCache("five", "12");
+//		controller.setCache("six", "12355");
+//		controller.setCache("seven", "5323");
+//		Thread.sleep(9000);
+//		System.out.println(controller.getAll());
+//		
+//		System.out.println(controller.getBucketCapacity());
+//		Thread.sleep(2000);
+//		System.out.println(controller.getAll());
+//		controller.setCache("first", "1");
+//		controller.setCache("second", "1");
+//		controller.setCache("third", "1");
+//		controller.setCache("four", "1");
+//		controller2.setCache("five", "1");
+//		controller2.setCache("first", "1");
 //		ConcurrentHashMap<String, Cache> xyz = new ConcurrentHashMap<String, Cache>();
 //		xyz.put("sats", controller);
 //		HashMap<String, Object> amk = new HashMap<String, Object>();
-//		for (Map.Entry<String, Cache> entry : xyz.entrySet()) {
+//		amk.put("av", "ass");
+//		System.out.println(amk.get("aq"));
+		//		for (Map.Entry<String, Cache> entry : xyz.entrySet()) {
 //		    String key = entry.getKey().toString();
 //		    Cache value = entry.getValue();
 //		    amk.put(key, value);
