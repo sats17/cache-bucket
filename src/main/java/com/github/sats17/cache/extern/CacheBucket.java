ce6129a669ae9dfc0ccbd43425dc64d528094d60
sats17 <satishkumbhar9999@gmail.com>
2020-12-15 12:25:08 +0530

    Added java doc for CacheBucket and Bucket

--- src/main/java/com/github/sats17/cache/extern/CacheBucket.java
+++ src/main/java/com/github/sats17/cache/extern/CacheBucket.java
@@ -3,7 +3,7 @@ package com.github.sats17.cache.extern;
 import java.util.Map;
 
 /**
- * Interface for cache bucket.
+ * Interface of cache bucket
  * 
  * @version 1.0.0
  * @author Sats17
@@ -12,89 +12,88 @@ import java.util.Map;
 public interface CacheBucket {
 
 	/**
-	 * This method stores cache into Cache Bucket.
+	 * Stores cache into Bucket with key and value
 	 * 
-	 * @param key   : Unique key for cache, key should not be null, blank and it's
-	 *              size should be less than equal to 10.
-	 * @param value : Cache value that can be any object, but should not be null.
-	 * @return void : This method returns nothing.
+	 * @param key Unique key for cache, key should not be null, blank and it's
+	 *              size should be less than equal to 10
+	 * @param value Cache value that can be any object, but should not be null
 	 * @throws IllegalArgumentException If key and value does not meets with given
 	 *                                  conditions
 	 */
 	public void setCache(String key, Object value);
 
 	/**
-	 * This method returns cache object from cache bucket for matching key.
+	 * Returns cache object from bucket with matching key
 	 * 
-	 * @param key : Unique cache key that present in cache bucket. Key should not be null, blank and it's
-	 *              size should be less than equal to 10.
-	 * @return Object : Cache Object.
-	 * @throws IllegalArgumentException  If key does not meet given conditions
+	 * @param key Unique cache key that present in cache bucket. Key should not be null, blank and it's
+	 *              size should be less than equal to 10
+	 * @return the cache value
+	 * @throws IllegalArgumentException  If key does not meets given conditions
 	 */
 	public Object getCache(String key);
 
 	/**
-	 * This method returns all cache from Cache bucket.
+	 * Returns all cache and it's related keys from bucket
 	 * 
-	 * @return Map<String, Object> : Map contains cache key and value.
+	 * @return {@link java.util.Map<String, Object>} contains cache key and value
 	 */
 	public Map<String, Object> getAll();
 
 	/**
-	 * This method returns bucket capacity.
+	 * Returns bucket capacity
 	 * 
-	 * @return int : bucket capacity.
+	 * @return bucket capacity
 	 */
 	public int getBucketCapacity();
 
 	/**
-	 * This method resize the bucket capacity.
+	 * Resize the bucket capacity
 	 * 
-	 * @param capacity : New capacity value, it should be between 1 to 200.
+	 * @param capacity New capacity value, it should be between 1 to 200.
 	 * @throws IllegalArgumentException if capacity does not meets required conditions.
 	 */
 	public void setBucketCapacity(int capacity);
 
 	/**
-	 * This method clears the cache for the given key, if it is presents in bucket.
+	 * Clears the cache for the given key
 	 * 
-	 * @param key : Unique cache key, key should not be null, blank and it's
+	 * @param key Unique cache key, key should not be null, blank and it's
 	 *              size should be less than equal to 10.
 	 * @throws IllegalArgumentException if key does not meets required conditions.
 	 */
 	public void clear(String key);
 
 	/**
-	 * This method Clear all cache from bucket.
+	 * Clears the all cache that present in bucket
 	 */
 	public void clear();
 
 	/**
-	 * This method returns the cache time to live of the Bucket.
+	 * Returns the cache time to live of the Bucket
 	 * 
-	 * @return long : Return TTL time in milliseconds.
+	 * @return TTL time in milliseconds
 	 */
 	public long getBucketTTL();
 
 	/**
-	 * Sets bucket time to live
+	 * Sets the bucket time to live
 	 * 
-	 * @param timeToLive : Cache TTL should be in milliseconds, value should be between 1 to 14400000.
+	 * @param timeToLive Cache TTL should be in milliseconds, value should be between 1 to 14400000.
 	 * @throws IllegalArgumentException if timeToLive does not meets required condition. 
 	 */
 	public void setBucketTTL(long timeToLive);
 
 	/**
-	 * This method returns total number of cache entries present in Bucket.
+	 * Provides total count of cache entries present in bucket
 	 * 
-	 * @return int : total count in integer for bucket.
+	 * @return total entries
 	 */
 	public int getTotalEntries();
 	
 	/**
-	 * Returns true if bucket has no cache present, otherwise false.
+	 * Provide information for bucket is empty is or not
 	 * 
-	 * @return boolean : true if bucket is empty, otherwise false.
+	 * @return true if bucket is empty, otherwise false
 	 */
 	public boolean isEmpty();
 
